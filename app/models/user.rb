f9c9ce934d3aa89d90377fe2c105643339d50d55
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-12-21 23:08:35 +0900

    feat: 各モデルをPostモデルに関連付けるため、アソシエーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
     validates :email, format: { with: /\A[\x01-\x7E]+\z/ }
     validates :password, format: { with: /\A(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-z0-9]{8,32})\z/ }, confirmation: true
   end
+
+  has_many :posts
 end
