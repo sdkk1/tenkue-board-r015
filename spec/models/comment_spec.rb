2070707b9ad1960b84cf49a23a8d63ecd81c6a82
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-12-21 23:19:33 +0900

    test: Commentモデルのテストコード実装

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -15,8 +15,14 @@ RSpec.describe Comment, type: :model do
     end
     context '新規投稿ができない場合' do
       it 'コメントの内容が入力されていない' do
+        comment.comment = nil
+        comment.valid?
+        expect(comment.errors.full_messages).to include "Comment can't be blank"
       end
       it 'コメント内容が141文字以上で入力されている' do
+        comment.comment = Faker::Lorem.characters(number: 141)
+        comment.valid?
+        expect(comment.errors.full_messages).to include "Comment is too long (maximum is 140 characters)"
       end
     end
   end
