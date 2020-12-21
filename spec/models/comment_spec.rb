536b38f592d60f3dce845663be7b9f7b3fe3865b
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-12-21 23:18:12 +0900

    test: Commentモデルテストのために、例を整理

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:comment) { build(:comment) }
+
+  describe 'コメント新規投稿' do
+    context '新規投稿ができる場合' do
+      it 'コメントの内容が入力されている' do
+      end
+      it 'コメント内容が140文字以内で入力されている' do
+      end
+    end
+    context '新規投稿ができない場合' do
+      it 'コメントの内容が入力されていない' do
+      end
+      it 'コメント内容が141文字以上で入力されている' do
+      end
+    end
+  end
 end
