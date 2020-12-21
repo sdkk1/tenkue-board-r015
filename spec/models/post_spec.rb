e35835f7218afa71922ac42f7671065903637fb2
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-12-21 23:11:52 +0900

    test: Postモデルのテストコード実装

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,14 +6,23 @@ RSpec.describe Post, type: :model do
   describe '掲示板新規投稿' do
     context '新規投稿ができる場合' do
       it '投稿の内容が入力されている' do
+        expect(post).to be_valid
       end
       it '内容が140文字以内で入力されている' do
+        post.content = Faker::Lorem.characters(number: 140)
+        expect(post).to be_valid
       end
     end
     context '新規投稿ができない場合' do
       it '投稿の内容が入力されていない' do
+        post.content = nil
+        post.valid?
+        expect(post.errors.full_messages).to include "Content can't be blank"
       end
       it '内容が141文字以上で入力されている' do
+        post.content = Faker::Lorem.characters(number: 141)
+        post.valid?
+        expect(post.errors.full_messages).to include "Content is too long (maximum is 140 characters)"
       end
     end
   end
