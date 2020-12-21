b46a5029f76435505223728a6e8da4344c1edf4e
katsuokaisao <isaoda27@gmail.com>
2020-12-21 16:08:52 +0900

    chore: デモデータに不備があったのでseeds.rbを変更した。

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
+user1 = User.create!(name: '信長', email: 'nobunaga@example.com', password: 'foobar', password_confirmation: 'foobar')
+user2 = User.create!(name: '秀吉', email: 'hideyoshi@example.com', password: 'foobar', password_confirmation: 'foobar')
 10.times do |n|
-  Post.create(user_id: 1, content: "test#{n}")
-  Post.create(user_id: 2, content: "test#{n}")
+  Post.create(user_id: user1.id, content: "test#{n+1}")
+  Post.create(user_id: user2.id, content: "test#{n+1}")
 end
