347bfdda6067761232ce327cc628695a7caf5ee2
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-12-21 23:10:51 +0900

    test: Postモデルテストのため、例を整理

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:post) { build(:post) }
+
+  describe '掲示板新規投稿' do
+    context '新規投稿ができる場合' do
+      it '投稿の内容が入力されている' do
+      end
+      it '内容が140文字以内で入力されている' do
+      end
+    end
+    context '新規投稿ができない場合' do
+      it '投稿の内容が入力されていない' do
+      end
+      it '内容が141文字以上で入力されている' do
+      end
+    end
+  end
 end
