2ace61800d064c9965f3c11be732e91bdbe5b114
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-12-21 22:33:27 +0900

    feat: ユーザー登録時に未入力の場合は登録できなくするため、バリデーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :name, :password_confirmation
+  end
 end
