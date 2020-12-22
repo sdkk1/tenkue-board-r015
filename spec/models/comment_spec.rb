e611fb2d36ad7886bfa2e95e9cc4859dfcc34ae2
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-12-22 14:46:21 +0900

    test: コメントに紐づくレコードが無い場合のテストをするため、'comment_spec'にテストを追加

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  let(:comment) { build(:comment) }
+  let(:user) { create(:user) }
+  let(:post) { create(:post) }
+  let(:comment) { build(:comment, user_id: user.id, post_id: post.id) }
 
   describe 'コメント新規投稿' do
     context '新規投稿ができる場合' do
@@ -23,6 +25,16 @@ RSpec.describe Comment, type: :model do
         comment.comment = Faker::Lorem.characters(number: 141)
         comment.valid?
         expect(comment.errors.full_messages).to include "コメント内容は140文字以内で入力してください"
+      end
+      it 'コメントに紐づくユーザーがいない' do
+        comment.user_id = nil
+        comment.valid?
+        expect(comment.errors.full_messages).to include "ユーザーを入力してください"
+      end
+      it 'コメントに紐づく投稿がいない' do
+        comment.post_id = nil
+        comment.valid?
+        expect(comment.errors.full_messages).to include "投稿を入力してください"
       end
     end
   end
