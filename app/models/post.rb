65a597af247c6c7a23c1579200ca3b2ad874890c
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-12-22 14:07:52 +0900

    feat: 'user_id'が無いと掲示板投稿できない様にするため、Postモデルにバリデーションを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ApplicationRecord
-  validates :content, presence: true, length: { maximum: 140 }
+  with_options precedence: true do
+    validates :content, length: { maximum: 140 }
+    validates :user_id
+  end
 
   belongs_to :user
   has_many :comments
