1303acbf5df2b81b1f0e155ff29ee6606aa72fc8
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-12-23 11:15:27 +0900

    refactor: バリデーションの正規表現を統一させるため、Userモデルの正規表現を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :name, :password_confirmation
-    validates :email, format: { with: /\A[\x01-\x7E][\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: { case_sensitive: true }
+    validates :email, format: { with: /\A[\x01-\x7E][a-z_0-9+\-.]+@[a-z0-9\-.]+\.[a-z]+\z/i }, uniqueness: { case_sensitive: true }
     validates :password, format: { with: /\A(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-z0-9]{8,32})\z/ }, confirmation: true
   end
 
