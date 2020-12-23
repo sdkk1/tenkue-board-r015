5651df7a0d3a74ed3d3eda096966bcff41318267
katsuokaisao <isaoda27@gmail.com>
2020-12-23 22:13:28 +0900

    fix: seedデータのユーザーpasswordが条件を満たしていなかったので修正した。

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-user1 = User.create!(name: '信長', email: 'nobunaga@example.com', password: 'foobar', password_confirmation: 'foobar')
-user2 = User.create!(name: '秀吉', email: 'hideyoshi@example.com', password: 'foobar', password_confirmation: 'foobar')
+user1 = User.create!(name: '信長', email: 'nobunaga@example.com', password: 'foobar123', password_confirmation: 'foobar123')
+user2 = User.create!(name: '秀吉', email: 'hideyoshi@example.com', password: 'foobar123', password_confirmation: 'foobar123')
 10.times do |n|
   Post.create(user_id: user1.id, content: "test#{n+1}")
   Post.create(user_id: user2.id, content: "test#{n+1}")
