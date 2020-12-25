062c0e2652fb5d94a864ed2b63666b87246e97ef
sdkk1 <fast.break.baske03@gmail.com>
2020-12-25 22:57:50 +0900

    refactor:check_guestメソッドをapplication_controller.rbへまとめるリファクタリング

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def check_guest
+    email = resource&.email || params[:user][:email].downcase
+    if email == 'guest@example.com'
+      redirect_to root_path, alert: 'ゲストユーザーの変更・削除はできません。'
+    end
+  end
+
   protected
   def configure_permitted_parameters
     if resource_class == User
