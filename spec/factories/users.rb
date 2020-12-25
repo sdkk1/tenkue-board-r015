ed234dd912f3e2612f497c84b57e62eb51374737
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-12-25 20:47:10 +0900

    test: Userモデルのパスワード生成がバリデーションに合わない時があったため、'factorybot'の記述を修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     name                  { Faker::Games::Pokemon.name }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 8, max_length: 32) }
+    password              { '1234567a' }
     password_confirmation { password }
   end
 end
