d266f786a42df75def25d1835de8e9f84f601104
katsuokaisao <isaoda27@gmail.com>
2020-12-26 11:56:57 +0900

    fix: 未ログイン時には投稿画面にアクセスできないようにした。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   def index
     @posts = Post.preload(:user).page(params[:page]).per(5).order(created_at: :desc)
   end
