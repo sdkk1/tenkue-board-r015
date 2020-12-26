fbdf608c3610e4cf5acddb578b12edb4d1540107
katsuokaisao <isaoda27@gmail.com>
2020-12-26 12:34:55 +0900

    feat: 掲示板投稿処理の実装(controller)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,20 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(content: post_params[:text], user_id: current_user.id)
+    if @post.save
+      flash[:success] = '投稿に成功しました'
+      redirect_to root_path
+    else
+      flash[:alert] = '投稿に失敗しました'
+      render 'new'
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:text)
+  end
 end
