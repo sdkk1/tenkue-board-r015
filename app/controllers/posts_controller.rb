8a988b8f68eaf373fd0113fa778f0b1ed66cb205
katsuokaisao <isaoda27@gmail.com>
2020-12-26 11:37:42 +0900

    feat: 投稿画面の作成とそのルーティングの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,8 @@ class PostsController < ApplicationController
   def index
     @posts = Post.preload(:user).page(params[:page]).per(5).order(created_at: :desc)
   end
+
+  def new
+    @post = Post.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
+  resources :posts, only: [:new, :create]
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     passwords: 'users/passwords'
