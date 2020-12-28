43a5daabd806d57f5e241a0ba7b7395d311af619
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-12-28 23:30:00 +0900

    test: 'Likeモデル'のテストコードを実装するための、例を整理

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '投稿いいね登録機能' do
+    context 'いいね登録ができる' do
+      it 'ログインユーザーによる投稿に対しての初めてのいいね登録' do
+      end
+    end
+    context 'いいね登録ができない場合' do
+      it '投稿に対しての2回目以上のいいね登録' do
+      end
+      it '未ログインユーザーによるいいね登録' do
+      end
+      it '存在しない投稿に対してのいいね登録' do
+      end
+    end
+  end
 end
