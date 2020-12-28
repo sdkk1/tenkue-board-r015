a4f6abd4407a55436d91b56753b40438f0c8ee46
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-12-28 21:54:39 +0900

    perf: いいね登録・解除ボタンを押した際に同じページにリダイレクトさせるため、'likesコントローラー'に'request.referer(1つ前のページのパス)'へのリダイレクトを追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,12 @@
 class LikesController < ApplicationController
   def create
     current_user.likes.create(post_id: params[:post_id])
+    redirect_to request.referer
   end
 
   def destroy
     @like = current_user.likes.find_by(post_id: params[:id])
     @like.destroy
+    redirect_to request.referer
   end
 end
