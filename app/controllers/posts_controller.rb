90987a4cfb86fd80aa0fdd67ebf2de5ac4720fc4
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-12-28 21:15:33 +0900

    perf: 投稿一覧ページ表示の際に'N+1問題'を発生させないため、'posts#index'のコードを修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.preload(:user).page(params[:page]).per(5).order(created_at: :desc)
+    @posts = Post.preload([:user, :likes]).page(params[:page]).per(5).order(created_at: :desc)
   end
 
   def show
