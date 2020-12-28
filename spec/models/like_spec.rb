5b814c2e4e72ab076ba351393e0a9f65f8906a5d
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-12-29 00:01:36 +0900

    test: 'Likeモデル'の正常系テストコードを実装するため、'like_spec.rb'に変数定義とテストコードを記述

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
+  let(:user) { create(:user) }
+  let(:post) { create(:post) }
+  let(:like) { build(:like, user_id: user.id, post_id: post.id) }
+
   describe '投稿いいね登録機能' do
     context 'いいね登録ができる' do
       it 'ログインユーザーによる投稿に対しての初めてのいいね登録' do
+        expect(like).to be_valid
       end
     end
     context 'いいね登録ができない場合' do
