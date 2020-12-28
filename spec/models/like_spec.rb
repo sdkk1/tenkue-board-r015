0948da9e3dbb498a61ff0c53365a86b50e432b98
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-12-29 00:06:50 +0900

    test: 'Likeモデル'のテストコード実装するため、異常系のテストコード記述

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -13,10 +13,20 @@ RSpec.describe Like, type: :model do
     end
     context 'いいね登録ができない場合' do
       it '投稿に対しての2回目以上のいいね登録' do
+        like.save
+        another_like = build(:like, user_id: user.id, post_id: post.id)
+        another_like.valid?
+        expect(another_like.errors.full_messages).to include "ユーザーはすでに存在します"
       end
       it '未ログインユーザーによるいいね登録' do
+        like.user_id = nil
+        like.valid?
+        expect(like.errors.full_messages).to include "ユーザーを入力してください"
       end
       it '存在しない投稿に対してのいいね登録' do
+        like.post_id = nil
+        like.valid?
+        expect(like.errors.full_messages).to include "投稿を入力してください"
       end
     end
   end
