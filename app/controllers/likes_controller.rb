bc3d5a752b8c83b875c6cbf236de38ee8e186037
Yasunori-aloha <70802846+Yasunori-aloha@users.noreply.github.com>
2020-12-29 13:15:18 +0900

    refactor: 'likesコントローラー'内で同じコードを複数回使用していた為、リダイレクト処理をメソッドとして切り出し

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,12 +3,18 @@ class LikesController < ApplicationController
 
   def create
     current_user.likes.create(post_id: params[:post_id])
-    redirect_to request.referer
+    redirect_current_page
   end
 
   def destroy
     @like = current_user.likes.find_by(post_id: params[:post_id])
     @like.destroy
+    redirect_current_page
+  end
+
+  private
+
+  def redirect_current_page
     redirect_to request.referer
   end
 end
