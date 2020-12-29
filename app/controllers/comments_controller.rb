a0ae5ea4cf65b8d537242a7c0cfa74739486ba6e
sdkk1 <fast.break.baske03@gmail.com>
2020-12-29 22:14:23 +0900

    perf:コメント投稿が成功した時、フラッシュメッセージが出るように仕様変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,10 +5,11 @@ class CommentsController < ApplicationController
     @comment = @post.comments.build(comment_params)
     @comment.user_id = current_user.id
     if @comment.save
+      flash[:notice] = "コメントを投稿しました！"
       redirect_back(fallback_location: root_path)
     else
+      flash[:alert] = "コメントを(140文字以内で)入力してください。"
       redirect_back(fallback_location: root_path)
-      flash[:alert] = "コメントを(140文字以内で)入力してください"
     end
   end
 
