85231f70e73f1487fb2a12fdec1ba49f7c49bc94
sdkk1 <fast.break.baske03@gmail.com>
2020-12-29 23:33:23 +0900

    refactor:commentコントローラーのリファクタリングを実施

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,9 @@
 class CommentsController < ApplicationController
+  before_action :set_post, only: [:create, :edit, :update]
+  before_action :set_comment, only: [:destroy, :edit, :update, :correct_user]
+  before_action :correct_user, only: [:destory, :edit, :update]
 
   def create
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
     @comment.user_id = current_user.id
     if @comment.save
@@ -14,7 +16,6 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @comment = Comment.find(params[:id])
     if @comment.destroy
       flash[:notice] = "コメントを削除しました！"
       redirect_back(fallback_location: root_path)
@@ -22,15 +23,11 @@ class CommentsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:post_id])
-    @comment = Comment.find(params[:id])
     @comments = @post.comments.eager_load(:user).order(created_at: :desc)
     render "posts/show"
   end
 
   def update
-    @post = Post.find(params[:post_id])
-    @comment = Comment.find(params[:id])
     if @comment.update(comment_params)
       flash[:notice] = "コメントを更新しました！"
       redirect_to post_url(@post)
@@ -41,6 +38,21 @@ class CommentsController < ApplicationController
   end
 
   private
+
+    def set_post
+      @post = Post.find(params[:post_id])
+    end
+
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
+    def correct_user
+      unless @comment.user_id == current_user.id
+        flash[:alert] = "コメントの投稿者のみ編集できます。"
+        redirect_to root_url
+      end
+    end
 
     def comment_params
       params.require(:comment).permit(:comment, :post_id, :user_id)
