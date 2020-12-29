a75fe7ffb663fb1ace72ed6fe7e6f9195314096b
sdkk1 <fast.break.baske03@gmail.com>
2020-12-29 22:33:53 +0900

    feat:コメント削除機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,6 +13,14 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+      flash[:notice] = "コメントを削除しました！"
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
   private
 
     def comment_params
